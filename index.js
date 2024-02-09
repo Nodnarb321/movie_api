11e278c59e22988c1d923ac7b51bf4f02ca68a61
Nodnarb321 <87585179+Nodnarb321@users.noreply.github.com>
2024-02-09 18:02:04 -0500

    mass push

--- index.js
+++ index.js
@@ -16,7 +16,18 @@ const Users = Models.User;
 const { check, validationResult } = require('express-validator');
 
 const cors = require('cors');
-app.use(cors());
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
+      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+      return callback(new Error(message ), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 let auth = require('./auth')(app);
 
@@ -449,6 +460,6 @@ async (req, res) => {
 
 const port = process.env.PORT || 8080;
 app.listen(port, '0.0.0.0',() => {
-    console.log('Listening on Port ' + port);
+ console.log('Listening on Port ' + port);
 });
 
--- models.js
+++ models.js
@@ -20,12 +20,28 @@ let movieSchema = mongoose.Schema({
 });
 
 let userSchema = mongoose.Schema({
-    Username: {type: String, required: true},
-    Password: {type: String, required: true},
-    Email: {type: String, required: true},
-    Birthday: Date,
-    FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie'}]
+  Username: {type: String, required: true},
+  Password: {type: String, required: true},
+  Email: {type: String, required: true},
+  Birthday: Date,
+  FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
 });
+
+userSchema.statics.hashPassword = (password) => {
+  return bcrypt.hashSync(password, 10);
+};
+
+userSchema.methods.validatePassword = function(password) {
+  return bcrypt.compareSync(password, this.Password);
+};
+
+userSchema.statics.hashPassword = (password) => {
+  return bcrypt.hashSync(password, 10);
+};
+
+userSchema.methods.validatePassword = function(password) {
+  return bcrypt.compareSync(password, this.Password);
+};
 
 userSchema.statics.hashPassword = (password) => {
     return bcrypt.hashSync(password, 10);
